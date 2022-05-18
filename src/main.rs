4be0ade0f05d8cd89a9a6815972ccfcbe401af9b
Tim Harding <tim@timharding.co>
2022-05-17 19:06:10 -0700

    Reading from network interface card

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buffer = [0u8; 1504];
+    let read_len = nic.recv(&mut buffer)?;
+    println!("read {} bytes: {:x?}", read_len, &buffer[..read_len]);
+    Ok(())
 }
