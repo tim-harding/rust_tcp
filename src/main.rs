8eda109ff26cad206c224374e8e4b18ee70ca78f
Tim Harding <tim@timharding.co>
2022-05-17 19:54:19 -0700

    Only receive IPV4 packets

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+const IPV4_PROTO: u16 = 0x0800;
+
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buffer = [0u8; 1504];
@@ -7,6 +9,9 @@ fn main() -> io::Result<()> {
         let read_len = nic.recv(&mut buffer)?;
         let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
         let proto = u16::from_be_bytes([buffer[2], buffer[3]]);
+        if proto != IPV4_PROTO {
+            continue;
+        }
         let data = &buffer[4..read_len];
         let data_len = read_len - 4;
         println!(
