18a9b26a7aaa8c24f351c09792aa4fcc069cba64
Tim Harding <tim@timharding.co>
2022-05-17 19:43:54 -0700

    Printing flags and proto from NIC header

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 
 cargo build --release
 sudo setcap cap_net_admin=eip target/release/tcp
 target/release/tcp & pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ use std::io;
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buffer = [0u8; 1504];
-    let read_len = nic.recv(&mut buffer)?;
-    println!("read {} bytes: {:x?}", read_len, &buffer[..read_len]);
-    Ok(())
+    loop {
+        let read_len = nic.recv(&mut buffer)?;
+        let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
+        let proto = u16::from_be_bytes([buffer[2], buffer[3]]);
+        let data = &buffer[4..read_len];
+        let data_len = read_len - 4;
+        println!(
+            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
+            data_len, flags, proto, data
+        );
+    }
 }
