44cd495a8bc2fc6bb7aa6e7e2ad757887130de22
Tim Harding <tim@timharding.co>
2022-05-17 19:24:16 -0700

    Don't need to switch shells to send packet

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 cargo build --release
 sudo setcap cap_net_admin=eip target/release/tcp
-target/release/tcp
+target/release/tcp & pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+wait $pid
