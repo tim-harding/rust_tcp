c02a054d4e30b526ec9518c03b0fae01ff1c9c74
Tim Harding <tim@timharding.co>
2022-06-15 20:54:55 -0700

    Parsing TCP packets

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+use etherparse::{IpNumber, Ipv4HeaderSlice, TcpHeaderSlice};
+
 const IPV4_PROTO: u16 = 0x0800;
 
 fn main() -> io::Result<()> {
@@ -15,20 +17,32 @@ fn main() -> io::Result<()> {
 
         let data = &buffer[4..read_length];
         let data_length = read_length - 4;
-        let packet = match etherparse::Ipv4HeaderSlice::from_slice(data) {
+        let ip_packet = match Ipv4HeaderSlice::from_slice(data) {
             Ok(packet) => packet,
             Err(e) => {
-                eprintln!("Ignoring weird packet: {}", e);
+                eprintln!("Ignoring weird IP packet: {}", e);
+                continue;
+            }
+        };
+
+        if ip_packet.protocol() != IpNumber::Tcp as u8 {
+            continue;
+        }
+
+        let tcp_packet = match TcpHeaderSlice::from_slice(&data[ip_packet.slice().len()..]) {
+            Ok(packet) => packet,
+            Err(e) => {
+                eprintln!("Ignoring weird TCP packet: {}", e);
                 continue;
             }
         };
 
         println!(
-            "{} -> {}, {} bytes of {}",
-            packet.source_addr(),
-            packet.destination_addr(),
-            packet.payload_len(),
-            packet.protocol(),
+            "{} -> {}, {} bytes of TCP to port {}",
+            ip_packet.source_addr(),
+            ip_packet.destination_addr(),
+            ip_packet.payload_len(),
+            tcp_packet.destination_port(),
         );
     }
 }
