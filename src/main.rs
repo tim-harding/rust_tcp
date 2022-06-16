28d54701635e269d3c4b7a4e0f849a701d580c9a
Tim Harding <tim@timharding.co>
2022-06-15 20:37:19 -0700

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buffer = [0u8; 1504];
     loop {
-        let read_len = nic.recv(&mut buffer)?;
-        let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
-        let proto = u16::from_be_bytes([buffer[2], buffer[3]]);
-        if proto != IPV4_PROTO {
+        let read_length = nic.recv(&mut buffer)?;
+        let ethernet_flags = u16::from_be_bytes([buffer[0], buffer[1]]);
+        let ethernet_protocol = u16::from_be_bytes([buffer[2], buffer[3]]);
+        if ethernet_protocol != IPV4_PROTO {
             continue;
         }
 
-        let data = &buffer[4..read_len];
-        let data_len = read_len - 4;
+        let data = &buffer[4..read_length];
+        let data_length = read_length - 4;
         let packet = match etherparse::Ipv4HeaderSlice::from_slice(data) {
             Ok(packet) => packet,
             Err(e) => {
@@ -24,11 +24,11 @@ fn main() -> io::Result<()> {
         };
 
         println!(
-            "payload_len: {}, protocol: {}, source_addr: {}, destination_addr: {}",
+            "{} -> {}, {} bytes of {}",
+            packet.source_addr(),
+            packet.destination_addr(),
             packet.payload_len(),
             packet.protocol(),
-            packet.source_addr(),
-            packet.destination_addr()
         );
     }
 }
