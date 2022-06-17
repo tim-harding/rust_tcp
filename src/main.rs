54f128abf890564f9846c0e4e4966b9005aa7a32
Tim Harding <tim@timharding.co>
2022-06-16 18:14:42 -0700

    Removing link headers from TUN requirements

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use etherparse::{IpHeader, IpNumber, Ipv4HeaderSlice, TcpHeaderSlice};
-use std::{collections::{HashMap, hash_map::Entry}, io, net::Ipv4Addr};
+use etherparse::{IpNumber, Ipv4HeaderSlice, TcpHeaderSlice};
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    io,
+    net::Ipv4Addr,
+};
 use tcp::{Connection, StateError};
 use thiserror::Error;
 use tun_tap::Iface;
 
 mod tcp;
-
-const IPV4_PROTO: u16 = 0x0800;
 
 #[derive(Debug, Error)]
 enum MainError {
@@ -24,17 +26,11 @@ struct Quad {
 
 fn main() -> Result<(), MainError> {
     let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
-    let mut nic = Iface::new("tun0", tun_tap::Mode::Tun)?;
-    let mut buffer = [0u8; 1504];
+    let mut nic = Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
+    let mut buffer = [0u8; 1500];
     loop {
         let read_length = nic.recv(&mut buffer)?;
-        let _ethernet_flags = u16::from_be_bytes([buffer[0], buffer[1]]);
-        let ethernet_protocol = u16::from_be_bytes([buffer[2], buffer[3]]);
-        if ethernet_protocol != IPV4_PROTO {
-            continue;
-        }
-
-        let buffer = &buffer[4..read_length];
+        let buffer = &buffer[..read_length];
         let ip_header = match Ipv4HeaderSlice::from_slice(buffer) {
             Ok(packet) => packet,
             Err(e) => {
